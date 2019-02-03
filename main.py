fdac79f33d890d6c904ba60332a01f6ca974c367
AlinaArkhipova <Alinastoboy@gmail.com>
2019-02-03 11:57:51 +0300

    Rest files

---
+++ main.py
@@ -0,0 +1,9 @@
+from lib import cashback
+from lib import bmi
+
+print('our program')
+#value = cashback(10_000) #вызов + передача параметров
+#print(value)
+print(cashback(10_000)) # alt + enter
+print(cashback(20_000))
+print(bmi(50,1.6))
